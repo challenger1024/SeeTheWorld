8c1af458008babdf77131aa99442803ec0ab1420
challenger1024 <127417636+challenger1024@users.noreply.github.com>
2025-10-22 21:02:08 +0800

    update capture

---
+++ src/capture.cpp
@@ -0,0 +1,20 @@
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    cv::VideoCapture cap(0); // /dev/video0
+    if(!cap.isOpened()) {
+        std::cerr << "无法打开摄像头！" << std::endl;
+        return -1;
+    }
+    cv::Mat frame;
+    cap >> frame;
+    if(frame.empty()) {
+        std::cerr << "拍照失败！" << std::endl;
+        return -1;
+    }
+    cv::imwrite("image.jpg", frame);
+    std::cout << "已保存图像 image.jpg" << std::endl;
+    return 0;
+}
