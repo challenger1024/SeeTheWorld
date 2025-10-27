49d6fdee83d268b5176e56039c216ff198103fee
challenger1024 <127417636+challenger1024@users.noreply.github.com>
2025-10-27 21:11:58 +0800

    speak

--- src/speak.cpp
+++ src/speak.cpp
@@ -2,10 +2,9 @@
 #include <string>
 #include"SeeTheWorld.h"
 
-int SeeTheWorld::speak(const std::string& text) {
+void SeeTheWorld::speak(const std::string& text) {
     // 指定使用 card 1, device 0（耳机输出）
     std::string cmd = "espeak-ng -v cmn-latn-pinyin -s 200 -p 60 \"" + text + "\" --stdout | aplay -D plughw:1,0";
 
     system(cmd.c_str());
-    return 0;
 }
